995681c112dad911e5ac234958664ce6b9839188
Jônatas Legal <jonataslegal@gmail.com>
2021-09-26 00:21:30 -0400

    atualizando uri da api de colaboradores

--- src/Api/Services/ColaboradorService.cs
+++ src/Api/Services/ColaboradorService.cs
@@ -7,7 +7,7 @@ namespace App.Services
 {
     public class ColaboradorService : ConsumirApi
     {
-        private readonly Uri uri = new Uri("https://gestao-de-ferias-api-colaborador.azurewebsites.net/colaborador");
+        private readonly Uri uri = new Uri("https://api-gestao-de-ferias-colaborador.azurewebsites.net/Colaborador");
 
         public async Task<List<ColaboradorDto>> Buscar()
         {
